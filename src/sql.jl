d62b740aa6af9732bbbbdc556ca64b130537809d
Andrei Zhabinski <faithlessfriend@gmail.com>
2017-01-28 02:15:46 +0300

    caching in SQLite

--- src/sql.jl
+++ src/sql.jl
@@ -2,35 +2,78 @@
 using SQLite
 using DataFrames
 
-const DB_PATH = "db.sqlite3"
+const DB_PATH = Pkg.dir("PackageTracker", "db.sqlite3")
+
+empty_package_dataframe() = DataFrame(name=String[], author=String[],
+                                      url=String[], pubdate=DateTime[],
+                                      description=String[])
+
 
 function to_dataframe(pkgs::Vector{NewPackage})
-    df = DataFrame(name=String[], author=String[], url=String[],
-                   pubdate=DateTime[], description=String[])
+    df = empty_package_dataframe()
     for pkg in pkgs
         push!(df, [pkg.name pkg.author pkg.url pkg.pubdate pkg.description])
     end
     return df
 end
 
+
 function from_dataframe(df::DataFrame)
     pkgs = Array{NewPackage}(0)
     for row in eachrow(df)
-        pkg = NewPackage(row[:name], row[:author], row[:url],
-                         row[:pubdate], row[:description])
+        pkg = NewPackage(get(row[:name]), get(row[:author]), get(row[:url]),
+                         get(row[:pubdate]), get(row[:description]))
         push!(pkgs, pkg)
     end
     return pkgs
 end
 
+
+function ensure_packages_table(db::SQLite.DB)
+    flds = fieldnames(NewPackage)
+    fld_str = join(flds, ",")
+    SQLite.execute!(db, "create table if not exists packages ($fld_str)")
+end
+
+
+function insert_package(db::SQLite.DB, pkg::NewPackage)
+    flds = fieldnames(pkg)
+    params = chop(repeat("?,", length(flds)))
+    stmt = SQLite.Stmt(db, "insert into packages values (?,?,?,?,?)")
+    for (i, fld) in enumerate(flds)
+        SQLite.bind!(stmt, i, getfield(pkg, fld))
+    end
+    SQLite.execute!(stmt)
+end
+
+function insert_packages(pkgs::Vector{NewPackage})
+    db = SQLite.DB(DB_PATH)
+    ensure_packages_table(db)
+    for pkg in pkgs
+        insert_package(db, pkg)
+    end
+end
+
+
 function save_packages(pkgs::Vector{NewPackage})
     db = SQLite.DB(DB_PATH)
+    ensure_packages_table(db)
     df = to_dataframe(pkgs)
     SQLite.load(db, "packages", df)
 end
 
-function load_package()
+function load_packages()
     db = SQLite.DB(DB_PATH)
-    df = SQLite.query(db, "select * from packages")
-    return from_dataframe(df)
+    try
+        df = SQLite.query(db, "select * from packages")
+        return from_dataframe(df)
+    catch ex
+        # table doesn't exist
+        if isa(ex, SQLite.SQLiteException)
+            return NewPackage[]
+        else
+            throw(ex)
+        end
+    end
+
 end
--- src/web.jl
+++ src/web.jl
@@ -4,14 +4,25 @@ using HttpServer
 handler = HttpHandler() do req::Request, resp::Response
     if startswith(req.resource, "/rss")
         return rss_feed(req)
+    elseif startswith(req.resource, "/update")
+        return update_rss(req)
     else
         return Response(404, "Not found")
     end
 end
 
 
+function update_rss(req::Request)
+    new_pkgs = retrieve_new_packages()
+    old_pkgs = Set(load_packages())
+    upd_pkgs = [pkg for pkg in new_pkgs if !in(pkg, old_pkgs)]
+    insert_packages(upd_pkgs)
+    return Response(200, "OK")
+end
+
 function rss_feed(req::Request)
-    pkgs = retrieve_new_packages()
+    pkgs = load_packages()
+    pkgs = sort(pkgs; by=pkg->pkg.pubdate, rev=true)
     xml = create_rss(pkgs)
     return Response(200, xml)
 end
